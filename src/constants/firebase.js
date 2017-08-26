ce16afec6efa92a4cbab356ee543f2149510e51a
심철환 <a@akaiv.com>
2017-08-27 03:39:08 +0900

    Init firebase with config

---
+++ src/constants/firebase.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase'
+import config from '../config/firebase'
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const database = firebase.database()
