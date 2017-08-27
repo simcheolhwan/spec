c9b55de0947bbd8c5ed6ceff3db3812b66de46f3
심철환 <a@akaiv.com>
2017-08-27 17:34:02 +0900

    Import reboot.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './components/App'
+import './style/reboot.css'
 
 ReactDOM.render(
   <Provider store={store}>
