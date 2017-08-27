2b12f065a5a6128dffcc7b1094341be2c986980c
심철환 <a@akaiv.com>
2017-08-27 20:14:14 +0900

    Show App after auth is checked

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
-import App from './components/App'
+import App from './containers/App'
 import './style/reboot.css'
 
 ReactDOM.render(
