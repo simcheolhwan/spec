6cc335ac130b584a8edbf236b0f1677691303d1b
심철환 <a@akaiv.com>
2017-08-27 19:06:53 +0900

    Add component for sign out

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Signin from '../containers/Signin'
+import Signout from '../containers/Signout'
 
 const App = () =>
   <Router>
     <main>
       <Route exact path="/signin" component={Signin} />
+      <Route exact path="/signout" component={Signout} />
     </main>
   </Router>
 
