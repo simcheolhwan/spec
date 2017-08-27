fba4143884d4d5e58e1b32c2e588ba0ad4557d7a
심철환 <a@akaiv.com>
2017-08-27 17:34:28 +0900

    Start to route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
-const App = () => null
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Signin from '../containers/Signin'
+
+const App = () =>
+  <Router>
+    <main>
+      <Route exact path="/signin" component={Signin} />
+    </main>
+  </Router>
 
 export default App
