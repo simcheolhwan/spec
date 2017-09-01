baff6b317177d421323d0242657c8331e47c9815
심철환 <a@akaiv.com>
2017-09-02 01:16:49 +0900

    Improve Settings

--- src/containers/Settings.jsx
+++ src/containers/Settings.jsx
@@ -14,7 +14,7 @@ const propTypes = {
 class Settings extends Component {
   constructor(props) {
     super(props)
-    this.state = { name: '', slug: '', slugified: '', status: '', error: {} }
+    this.state = { name: '', slug: '', status: '', error: {} }
     this.handleInputChange = this.handleInputChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
@@ -25,41 +25,40 @@ class Settings extends Component {
     database
       .ref(`/users/${user.uid}`)
       .once('value', snap =>
-        this.setState({
-          ...snap.val(),
-          slugified: snap.val().slug,
-          status: 'fetched'
-        })
+        this.setState({ ...snap.val(), status: 'fetched' })
       )
       .catch(error => this.setState({ status: 'error', error }))
   }
 
   handleInputChange(event) {
     const { name, value } = event.target
-
-    if (name === 'slug') {
-      const slugified = slugify(value)
-      this.setState({ slugified })
-    }
-
     this.setState({ [name]: value })
   }
 
   handleSubmit(event) {
     const { user } = this.props
-    const { name, slugified } = this.state
-
+    const { name, slug, disabled } = this.sanitize()
     event.preventDefault()
-    slugified &&
-      database
-        .ref(`/users/${user.uid}`)
-        .set({ name, slug: slugified })
-        .then(() => this.setState({ status: 'done' }))
-        .catch(error => this.setState({ error, status: 'error' }))
+    !disabled &&
+      this.setState({ status: 'submitting' }, () =>
+        database
+          .ref(`/users/${user.uid}`)
+          .set({ name, slug })
+          .then(() => this.setState({ status: 'done' }))
+          .catch(error => this.setState({ status: 'error', error }))
+      )
+  }
+
+  sanitize() {
+    const { name: _name, slug: _slug } = this.state
+    const name = _name.trim()
+    const slug = slugify(_slug)
+    return { name, slug, disabled: !(name && slug) }
   }
 
   render() {
-    const { name, slug, slugified, status, error } = this.state
+    const { name, slug, status, error } = this.state
+    const { disabled } = this.sanitize()
     const fields = {
       Name: { value: name, autoFocus: true },
       Slug: { value: slug }
@@ -78,11 +77,14 @@ class Settings extends Component {
           />
         ))}
 
-        <code>{slugified}</code>
+        <code>{slugify(slug)}</code>
 
-        <button type="submit">Submit</button>
+        <button type="submit" disabled={disabled}>
+          Submit
+        </button>
 
-        {error.message || (status === 'done' && '✔︎')}
+        {error.message ||
+          (status === 'submitting' ? '🔄' : status === 'done' && '✅')}
       </form>
     ) : null : (
       <Redirect to="/signin" />
