843bf41fea89cba5a2b36ab2666660a05617af89
심철환 <a@akaiv.com>
2017-09-03 01:36:37 +0900

    Copy content of ProjectCreate to ProjectUpdate

--- src/containers/ProjectUpdate.jsx
+++ src/containers/ProjectUpdate.jsx
@@ -1,9 +1,89 @@
-const propTypes = {}
-const defaultProps = {}
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import { createProject } from '../actions/project'
+import { slugify } from '../helpers/utils'
+import Form from '../components/Form'
 
-const ProjectUpdate = () => null
+const propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  onCreate: PropTypes.func.isRequired
+}
+
+class ProjectUpdate extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      title: '',
+      slug: '',
+      isPrivate: false,
+      status: '',
+      error: {}
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleInputChange(event) {
+    const { name, type, value, checked } = event.target
+    this.setState({ [name]: type === 'checkbox' ? checked : value })
+  }
+
+  handleSubmit(event) {
+    const { title, slug, disabled } = this.sanitize()
+    const { isPrivate } = this.state
+    const { onCreate } = this.props
+
+    event.preventDefault()
+    !disabled &&
+      this.setState({ status: 'submitting' }, () =>
+        onCreate({ title, slug, isPrivate })
+      )
+  }
+
+  sanitize() {
+    const { title: _title, slug: _slug } = this.state
+    const title = _title.trim()
+    const slug = slugify(_slug)
+    return { title, slug, disabled: !(title && slug) }
+  }
+
+  render() {
+    const { title, slug, isPrivate, status, error } = this.state
+    const { disabled } = this.sanitize()
+
+    const fields = {
+      Title: { type: 'text', value: title, autoFocus: true },
+      Slug: { type: 'text', value: slug },
+      Private: { type: 'checkbox', name: 'isPrivate', checked: isPrivate }
+    }
+
+    const formProps = {
+      fields,
+      disabled,
+      errorMessage: error.message,
+      isSubmitting: status === 'submitting',
+      isDone: status === 'done',
+      onInputChange: this.handleInputChange,
+      onSubmit: this.handleSubmit
+    }
+
+    return this.props.authenticated ? (
+      <Form {...formProps} />
+    ) : (
+      <Redirect to="/signin" />
+    )
+  }
+}
 
 ProjectUpdate.propTypes = propTypes
-ProjectUpdate.defaultProps = defaultProps
 
-export default ProjectUpdate
+const mapStateToProps = ({ auth }) => ({ authenticated: auth.authenticated })
+const mapDispatchToProps = dispatch => ({
+  onCreate: project => dispatch(createProject(project))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectUpdate)
