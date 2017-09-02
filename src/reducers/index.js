c0750a072e1216c571de890d5c67ed14d06c302f
심철환 <a@akaiv.com>
2017-09-03 04:29:03 +0900

    Add redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import app from './app'
 import auth from './auth'
 import user from './user'
 import projects from './projects'
 
-export default combineReducers({ app, auth, user, projects })
+export default combineReducers({ app, auth, user, projects, form })
