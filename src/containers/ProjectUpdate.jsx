788057134e74c21742fc3438c2648e2b2951b640
심철환 <a@akaiv.com>
2017-09-03 01:35:51 +0900

    Route ProjectUpdate

---
+++ src/containers/ProjectUpdate.jsx
@@ -0,0 +1,9 @@
+const propTypes = {}
+const defaultProps = {}
+
+const ProjectUpdate = () => null
+
+ProjectUpdate.propTypes = propTypes
+ProjectUpdate.defaultProps = defaultProps
+
+export default ProjectUpdate
