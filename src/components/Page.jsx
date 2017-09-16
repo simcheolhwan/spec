27ba54b8a76aa2e26266069eb70f840ba51b3507
심철환 <a@akaiv.com>
2017-09-17 02:02:04 +0900

    Improve Page to present actions

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -4,18 +4,21 @@ import { colors } from '../styles'
 
 const propTypes = {
   title: PropTypes.string,
+  actions: PropTypes.arrayOf(PropTypes.node),
   children: PropTypes.node
 }
 
 const defaultProps = {
   title: '',
+  actions: [],
   children: null
 }
 
-const Page = ({ title, children }) => (
+const Page = ({ title, actions, children }) => (
   <article style={style.Page}>
     <header style={style.header}>
       <h1>{title}</h1>
+      {!!actions.length && <section>{actions}</section>}
     </header>
 
     {children}
@@ -31,6 +34,9 @@ const style = {
   },
 
   header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottom: `1px solid ${colors.line}`,
     marginBottom: '1rem',
     paddingBottom: '.5rem'
