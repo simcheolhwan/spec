acadcbc9d17f25fd94cb253a447291e0f2be7a41
심철환 <a@akaiv.com>
2017-10-02 04:02:07 +0900

    Add component NotFound

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => <article style={style}>Not Found</article>
+
+const style = {
+  fontSize: '2rem',
+  padding: '2rem',
+  textAlign: 'center'
+}
+
+export default NotFound
