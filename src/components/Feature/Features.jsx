926ffabe0526ee26fd93f0700974eba40210678e
심철환 <a@akaiv.com>
2017-10-03 01:46:58 +0900

    Fetch features logically on Project

--- src/components/Feature/Features.jsx
+++ src/components/Feature/Features.jsx
@@ -1,10 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 import { getProject } from '../../utils'
-import { fetchFeatures } from '../../actions/featureActions'
-import { fetchSpecs } from '../../actions/specActions'
 import Page from '../Page'
 import Feature from './Feature'
 import FeatureCreate from './FeatureCreate'
@@ -12,52 +9,32 @@ import FeatureCreate from './FeatureCreate'
 const propTypes = {
   features: PropTypes.object.isRequired,
   specs: PropTypes.object.isRequired,
-  user: PropTypes.object.isRequired,
-  projectKey: PropTypes.string.isRequired,
-  fetchFeatures: PropTypes.func.isRequired,
-  fetchSpecs: PropTypes.func.isRequired
+  projectKey: PropTypes.string.isRequired
 }
 
-class Features extends Component {
-  componentWillMount() {
-    const { user: { uid }, projectKey, fetchFeatures, fetchSpecs } = this.props
-    fetchFeatures(uid, projectKey)
-    fetchSpecs(uid, projectKey)
-  }
-
-  render() {
-    const { features, specs, projectKey } = this.props
-
-    return (
-      <Page
-        title="Features"
-        actions={[
-          <FeatureCreate projectKey={projectKey} key="Featurecreate" />
-        ]}
-      >
-        {features.order.map(key => (
-          <Feature
-            projectKey={projectKey}
-            featureKey={key}
-            feature={features.list[key]}
-            specs={{ list: specs.list, order: specs.orders[key] || [] }}
-            key={key}
-          />
-        ))}
-      </Page>
-    )
-  }
-}
+const Features = ({ features, specs, projectKey }) => (
+  <Page
+    title="Features"
+    actions={[<FeatureCreate projectKey={projectKey} key="Featurecreate" />]}
+  >
+    {features.order.map(key => (
+      <Feature
+        projectKey={projectKey}
+        featureKey={key}
+        feature={features.list[key]}
+        specs={{ list: specs.list, order: specs.orders[key] || [] }}
+        key={key}
+      />
+    ))}
+  </Page>
+)
 
 Features.propTypes = propTypes
 
-const mapStateToProps = (state, ownProps) => ({
-  ...getProject(state, ownProps),
-  features: state.features,
-  specs: state.specs
+const mapStateToProps = ({ projects, features, specs }, ownProps) => ({
+  ...getProject(ownProps)(projects),
+  features,
+  specs
 })
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ fetchFeatures, fetchSpecs }, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Features)
+export default connect(mapStateToProps)(Features)
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,11 @@
-import isEmpty from 'lodash/fp/isEmpty'
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Switch, Route, Link } from 'react-router-dom'
-import { getProject } from '../../utils'
+import { fetchFeatures } from '../../actions/featureActions'
+import { fetchSpecs } from '../../actions/specActions'
+import { match, getProject } from '../../utils'
 import { colors } from '../../styles'
 import NotFound from '../NotFound'
 import Grid from '../Grid'
@@ -11,56 +13,89 @@ import ProjectUpdate from './ProjectUpdate'
 import Features from '../Feature/Features'
 
 const propTypes = {
+  state: PropTypes.oneOf(['idle', 'project', 404]),
   project: PropTypes.object,
-  isOwned: PropTypes.bool.isRequired
+  isOwner: PropTypes.bool.isRequired,
+  action: PropTypes.shape({
+    fetchFeatures: PropTypes.bool.isRequired
+  }).isRequired,
+  fetchFeatures: PropTypes.func.isRequired,
+  fetchSpecs: PropTypes.func.isRequired
 }
 
 const defaultProps = {
   project: {}
 }
 
-const Project = ({ project, isOwned, user: { name, slug }, match }) => {
-  const breadcrumb = {
-    user: (
-      <Link to={'/' + slug} style={style.Link}>
-        {name}
-      </Link>
-    ),
-
-    project: (
-      <Link to={match.url} style={style.Link}>
-        {project.title}
-      </Link>
-    )
+class Project extends Component {
+  componentWillMount() {
+    this.fetch(this.props)
   }
 
-  const menu = {
-    setting: (
-      <Link to={match.url + '/settings'} style={style.Link}>
-        Setting
-      </Link>
-    )
+  componentWillReceiveProps(nextProps) {
+    this.fetch(nextProps)
   }
 
-  const grid = {
-    aside: (
-      <nav>
-        <h1>
-          {breadcrumb.user}/{breadcrumb.project}
-        </h1>
-        {isOwned && menu.setting}
-      </nav>
-    ),
-
-    main: (
-      <Switch>
-        <Route path={match.path} exact component={Features} />
-        <Route path={match.path + '/settings'} component={ProjectUpdate} />
-      </Switch>
-    )
+  fetch = ({ user, projectKey, action, fetchFeatures, fetchSpecs }) => {
+    const { uid } = user
+    action.fetchFeatures && fetchFeatures(uid, projectKey)
+    action.fetchFeatures && fetchSpecs(uid, projectKey)
   }
 
-  return isEmpty(project) ? <NotFound /> : <Grid {...grid} />
+  render() {
+    const { state, action, project, isOwner, user, match } = this.props
+    const { name, slug } = user
+
+    const breadcrumb = {
+      user: (
+        <Link to={'/' + slug} style={style.Link}>
+          {name}
+        </Link>
+      ),
+
+      project: (
+        <Link to={match.url} style={style.Link}>
+          {project.title}
+        </Link>
+      )
+    }
+
+    const menu = {
+      setting: (
+        <Link to={match.url + '/settings'} style={style.Link}>
+          Setting
+        </Link>
+      )
+    }
+
+    const grid = {
+      aside: (
+        <nav>
+          <h1>
+            {breadcrumb.user}/{breadcrumb.project}
+          </h1>
+          {isOwner && menu.setting}
+        </nav>
+      ),
+
+      main: (
+        <Switch>
+          {!action.fetchFeatures && (
+            <Route path={match.path} exact component={Features} />
+          )}
+          <Route path={match.path + '/settings'} component={ProjectUpdate} />
+        </Switch>
+      )
+    }
+
+    const ui = {
+      idle: null,
+      project: <Grid {...grid} />,
+      404: <NotFound />
+    }
+
+    return ui[state]
+  }
 }
 
 Project.propTypes = propTypes
@@ -70,6 +105,20 @@ const style = {
   Link: { color: colors.black }
 }
 
-const mapStateToProps = (state, ownProps) => getProject(state, ownProps)
+const mapStateToProps = (_state, ownProps) => {
+  const { auth, user: _user, projects, features } = _state
+  const isOwner = match(ownProps)(auth)
+  const { user } = isOwner ? auth : _user
+  const { projectKey, project } = getProject(ownProps)(projects)
+  const matchProject = projectKey === features.project
+  const state =
+    projects.state === 'idle' ? 'idle' : projectKey ? 'project' : 404
+  const action = { fetchFeatures: !matchProject && !!user.uid && !!projectKey }
 
-export default connect(mapStateToProps)(Project)
+  return { state, action, user, projectKey, project, isOwner }
+}
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ fetchFeatures, fetchSpecs }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Project)
