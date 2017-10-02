090e1554f932c9fb878c0863da1d35e3322ce0b9
심철환 <a@akaiv.com>
2017-10-03 02:51:51 +0900

    Fetch projects on ProjectCreate

--- src/components/Project/ProjectCreate.jsx
+++ src/components/Project/ProjectCreate.jsx
@@ -1,41 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
-import { createProject } from '../../actions/projectActions'
+import { fetchProjects, createProject } from '../../actions/projectActions'
 import { sanitize } from '../../utils'
 import Page from '../Page'
 import Form from './ProjectForm'
 
 const propTypes = {
-  state: PropTypes.oneOf(['guest', 'user']).isRequired,
+  state: PropTypes.oneOf(['idle', 'guest', 'user']).isRequired,
   createProject: PropTypes.func.isRequired
 }
 
-const ProjectCreate = ({ state, createProject }) => {
-  const submit = project => createProject(sanitize(project))
-
-  const ui = {
-    guest: <Redirect to="/signin" />,
-    user: (
-      <Page title="Create a new project">
-        <Form onSubmit={submit} submitButton="Create project" />
-      </Page>
-    )
+class ProjectCreate extends Component {
+  componentWillMount() {
+    const { action, user, fetchProjects } = this.props
+    action.fetchProjects && fetchProjects(user.uid)
   }
 
-  return ui[state]
+  submit = project => {
+    this.props.createProject(sanitize(project))
+  }
+
+  render() {
+    const { state } = this.props
+
+    const ui = {
+      idle: null,
+      guest: <Redirect to="/signin" />,
+      user: (
+        <Page title="Create a new project">
+          <Form onSubmit={this.submit} submitButton="Create project" />
+        </Page>
+      )
+    }
+
+    return ui[state]
+  }
 }
 
 ProjectCreate.propTypes = propTypes
 
-const mapStateToProps = ({ auth: { state }, projects: { error } }) => ({
-  state,
-  error
-})
+const mapStateToProps = ({ auth, projects }) => {
+  const matchProjects = auth.user.uid === projects.user
+
+  return {
+    state: matchProjects ? auth.state : 'idle',
+    action: { fetchProjects: !matchProjects },
+    user: auth.user,
+    error: projects.error
+  }
+}
 
 const mapDispatchToProps = dispatch =>
-  bindActionCreators({ createProject }, dispatch)
+  bindActionCreators({ fetchProjects, createProject }, dispatch)
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProjectCreate)
