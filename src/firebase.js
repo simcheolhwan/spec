6e8439236006a451830833697d20b80a63f4676c
심철환 <a@akaiv.com>
2017-10-06 15:10:32 +0900

    Set firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase'
+import 'firebase/firestore'
 import config from './config/firebase'
 
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
-export const database = firebase.database()
+export const database = firebase.firestore()
