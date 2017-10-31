2a9b41ec7476ad5090dbf834a5f6d63ffed64eeb
심철환 <a@akaiv.com>
2017-11-01 00:22:20 +0900

    Feature(Features): Enable to create Feature as a Issue

--- src/components/Feature/FeatureCreate.jsx
+++ src/components/Feature/FeatureCreate.jsx
@@ -5,21 +5,28 @@ import { connect } from 'react-redux'
 import { createFeature } from '../../actions/featureActions'
 
 const propTypes = {
+  isIssue: PropTypes.bool,
   projectKey: PropTypes.string.isRequired,
   createFeature: PropTypes.func.isRequired
 }
 
-const FeatureCreate = ({ projectKey, createFeature }) => {
+const defaultProps = {
+  isIssue: false
+}
+
+const FeatureCreate = ({ isIssue, projectKey, createFeature }) => {
   const create = () => {
     const _name = window.prompt('Type a feature name') || ''
     const name = _name.trim()
-    name && createFeature(projectKey, { name })
+    const feature = Object.assign({ name }, isIssue && { issue: true })
+    name && createFeature(projectKey, feature)
   }
 
   return <button onClick={create}>New feature</button>
 }
 
 FeatureCreate.propTypes = propTypes
+FeatureCreate.defaultProps = defaultProps
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators({ createFeature }, dispatch)
